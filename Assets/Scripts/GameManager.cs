eb07d196c5d24e47e8ccb6ebaccb4e88ebaa0807
minhnhan253 <nminhnhan@gmail.com>
2020-02-07 11:25:52 +0700

    added gamemanager, board Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    private int level = 3;
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupSence(level);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
